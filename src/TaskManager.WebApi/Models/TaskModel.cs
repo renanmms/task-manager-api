7b88de2f069dc148f0bc47c4ee688f350c5c27db
Renan Martins <renanmartins1999@hotmail.com>
2026-03-10 21:41:13 -0300

    feat: add id to TaskModel

--- src/TaskManager.WebApi/Models/TaskModel.cs
+++ src/TaskManager.WebApi/Models/TaskModel.cs
@@ -2,6 +2,7 @@ namespace TaskManager.WebApi.Models
 {
     public class TaskModel(string title)
     {
+        public int Id { get; set; }
         public string Title { get; set; } = title;
         public string? Description { get; set; }
         public DateTime? ExpiresAt { get; set; }
