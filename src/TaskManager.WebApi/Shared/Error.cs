d25309180df248e9e00c4d3141afe38ad661a85d
Renan Martins <renanmartins1999@hotmail.com>
2026-03-12 16:42:37 -0300

    feat: add error type to error class

--- src/TaskManager.WebApi/Shared/Error.cs
+++ src/TaskManager.WebApi/Shared/Error.cs
@@ -2,8 +2,29 @@ using System;
 
 namespace TaskManager.WebApi.Shared
 {
-    public sealed record Error(string Code, string Description)
+    public enum ErrorType
     {
+        Validation,
+        NotFound
+    }
+
+    public sealed class Error
+    {
+        public string? Code { get; set; }
+        public string Description { get; set; }
+        public ErrorType ErrorType { get; set; }
+        public Error(string code, string description)
+        {
+            Code = code;
+            Description = description;
+        }
+
+        public Error(ErrorType errorType, string description)
+        {
+            ErrorType = errorType;
+            Description = description;
+        }
+
         public static readonly Error None = new(string.Empty, string.Empty);
     }   
 }
